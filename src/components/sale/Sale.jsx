8fab694a4c873a9c09f23c13b1eb255eedef8dee
vilkov <vilkovaleksandr@gmail.com>
2023-05-01 23:36:28 +0300

    commit

--- src/components/sale/Sale.jsx
+++ src/components/sale/Sale.jsx
@@ -1,18 +1,23 @@
 import React, {useCallback, useEffect, useState} from 'react';
 import './Sale.css';
-import {useTelegram} from "../../hooks/useTelegram.js";
-import ConcernItem from "../items/concernItem/СoncernItem.jsx";
-import BrandItem from "../items/brandItem/BrandItem.jsx";
-import ModelItem from "../items/modelItem/ModelItem.jsx";
-import CarItem from "../items/carItem/CarItem.jsx";
+import Select from "../select/select.jsx";
 
 const Sale = () => {
     const [data, setData] = useState('')
 
+    const [concern, setConcern] = useState('')
+    const [brand, setBrand] = useState('')
+    const [model, setModel] = useState('')
+    const [engine, setEngine] = useState('')
+
+    const [concernsArr, setConcernsArr] = useState([]);
+    const [brandsArr, setBrandsArr] = useState([]);
+    const [modelsArr, setModelArr] = useState([]);
+    const [enginesArr, setEnginesArr] = useState([]);
+
     const fetchData = () => {
         fetch('http://localhost:8080/car/allCars', {
             method: 'GET',
-            //mode: 'no-cors',
             headers: {
                 'Content-Type': 'application/json',
                 'Accept': 'application/json',
@@ -30,97 +35,144 @@ const Sale = () => {
     }, [])
 
 
-    // const concernData = [
-    //     "PSA"
-    // ]
-    // const brandData = [
-    //     "PEUGEOT", "CITROEN"
-    // ]
-    //
-    // const modelData = [
-    //     "C4", "307"
-    // ]
-    //
-    // const [concern, setConcern] = useState('')
+    useEffect(() => {
+        if (data) {
+            const concerns = new Set();
+            data.forEach(v => concerns.add(v.concern.name));
+            setConcernsArr(Array.from(concerns));
+            if (!concern) {
+                setConcern(concernsArr[0])
+            }
+        }
+    }, [data])
+
+    useEffect(() => {
+        if (data && concern) {
+            const brands = new Set();
+            data.forEach(v => {
+                if (v.concern.name === concern) {
+                    brands.add(v.brand.name)
+                }
+            })
+            setBrandsArr(Array.from(brands));
+        }
+    }, [data, concern])
+
+    useEffect(() => {
+        if (data && concern && brand) {
+            const models = new Set();
+            data.forEach(e => {
+                if (e.concern.name === concern && e.brand.name === brand) {
+                    models.add(e.model.name)
+                }
+            })
+            setModelArr(Array.from(models));
+        }
+    }, [data, brand])
 
 
-    //
-    // const [price, setPrice] = useState('')
-    // const [description, setDescription] = useState('')
-    // const {tg} = useTelegram()
-    //
-    //
-    //
-    // const onSendData = useCallback(() => {
-    //     const data = {
-    //         concern,
-    //         brand,
-    //         model,
-    //         price,
-    //         description,
-    //         action: "SALE"
-    //     }
-    //     tg.sendData(JSON.stringify(data));
-    // }, [concern, brand, model, engine, price, description])
-    //
-    // useEffect(() => {
-    //     tg.onEvent("mainButtonClicked", onSendData)
-    //     return () => {
-    //         tg.offEvent("mainButtonClicked", onSendData)
-    //     }
-    // }, [onSendData])
-    //
-    // useEffect(() => {
-    //     tg.MainButton.setParams({
-    //         text: "Продать"
-    //     })
-    // }, [])
-    //
-    // //Валидация кнопки
-    // useEffect(() => {
-    //     if (!price || !description) {
-    //         tg.MainButton.hide();
-    //     } else {
-    //         tg.MainButton.show();
-    //     }
-    //
-    // }, [price, description])
-    //
-    let updateData;
-    updateData = (value) => {
-        this.setState({name: value})
-    }
+    useEffect(() => {
+        if (data && concern && brand && model) {
+            const engines = new Set();
+            data.forEach(e => {
+                if (e.concern?.name === concern && e.brand?.name === brand && e.model?.name === model) {
+                    engines.add(e.engine.name)
+                    debugger
+                }
+            })
+            setEnginesArr(Array.from(engines));
+        }
+    }, [data, model])
 
-    const [concern, setConcern] = useState('')
-    const [brand, setBrand] = useState('')
-    const [model, setModel] = useState('')
-    const [car, setCar] = useState('')
+
     const handleConcern = (concern) => {
         setConcern(concern)
         setBrand(null)
         setModel(null)
+        setEngine(null)
     }
+
     const handleBrand = (e) => {
         setBrand(e)
         setModel(null)
+        setEngine(null)
     }
 
     const handleModel = (e) => {
+        debugger
         setModel(e)
     }
-    const handleCar = (e) => {
-        setCar(e)
+    const handleEngine = (e) => {
+        setEngine(e)
     }
     return (
         <div className={"sale"}>
             <h3>Ты находишься на страничке создания объявления</h3>
             <h3>Ты выбрал следующие данные: {concern} {brand} {model}</h3>
-            <ConcernItem data={data} onChange={handleConcern}/>
-            <BrandItem concern={concern} data={data} onChange={handleBrand}/>
-            <ModelItem concern={concern} data={data} brand={brand} onChange={handleModel}/>
-            <CarItem concern={concern} data={data} brand={brand} model={model} onChange={handleCar}/>
+            <Select label={"concern"} values={concernsArr} onChange={handleConcern}/>
+            <Select label={"Brand"} values={brandsArr} onChange={handleBrand}/>
+            <Select label={"model"} values={modelsArr} onChange={handleModel}/>
+            <Select label={"car"} values={enginesArr} onChange={handleEngine}/>
         </div>
     );
 };
 
 export default Sale;
+
+
+// const concernData = [
+//     "PSA"
+// ]
+// const brandData = [
+//     "PEUGEOT", "CITROEN"
+// ]
+//
+// const modelData = [
+//     "C4", "307"
+// ]
+//
+// const [concern, setConcern] = useState('')
+
+
+//
+// const [price, setPrice] = useState('')
+// const [description, setDescription] = useState('')
+// const {tg} = useTelegram()
+//
+//
+//
+// const onSendData = useCallback(() => {
+//     const data = {
+//         concern,
+//         brand,
+//         model,
+//         price,
+//         description,
+//         action: "SALE"
+//     }
+//     tg.sendData(JSON.stringify(data));
+// }, [concern, brand, model, engine, price, description])
+//
+// useEffect(() => {
+//     tg.onEvent("mainButtonClicked", onSendData)
+//     return () => {
+//         tg.offEvent("mainButtonClicked", onSendData)
+//     }
+// }, [onSendData])
+//
+// useEffect(() => {
+//     tg.MainButton.setParams({
+//         text: "Продать"
+//     })
+// }, [])
+//
+// //Валидация кнопки
+// useEffect(() => {
+//     if (!price || !description) {
+//         tg.MainButton.hide();
+//     } else {
+//         tg.MainButton.show();
+//     }
+//
+// }, [price, description])
+//
---
+++ src/components/select/select.jsx
@@ -0,0 +1,35 @@
+import React, {useEffect, useState} from 'react';
+import './select.css'
+
+
+/*
+* props.values
+* props.onChange(value)
+* props.label
+* */
+const Select = (props) => {
+    const [value, setValue] = useState();
+    const [values, setValues] = useState([]);
+
+    const options = values.map((text, index) => {
+        return <option key={text} value={text}>{text}</option>;
+    });
+
+    useEffect(() => {
+        setValues(props.values)
+    },[props])
+    const handleChange = (value) => {
+        setValue(value)
+        props.onChange(value) // callback-функция
+    }
+    return (
+        <div className={"select"}>
+            <h3>{props.label}</h3>
+            <select value={value} onChange={(event) => handleChange(event.target.value)}>
+                {options}
+            </select>
+        </div>
+    );
+};
+
+export default Select;
