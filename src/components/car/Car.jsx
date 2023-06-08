7a106ab47d307c7179bf46f08e06d13db613daea
vilkov <alexander.vilkov@gridnine.com>
2023-06-08 17:37:06 +0300

    web app

--- src/components/car/Car.jsx
+++ src/components/car/Car.jsx
@@ -158,7 +158,7 @@ const Car = (props) => {
 
     useEffect(() => {
         if (result) {
-            props.handleCars(JSON.stringify(result))
+            props.handleCars(result)
         }
     }, [result])
 
